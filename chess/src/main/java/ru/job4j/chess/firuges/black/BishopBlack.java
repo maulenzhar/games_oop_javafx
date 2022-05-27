3e796fa9e3aa323ab0413b67faaa1ea5ee4ab3d4
mazajaja <you@example.com>
2022-05-27 22:33:23 +0600

    2. Каркас шахматной доски [#792 #305615]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -27,27 +27,20 @@ public class BishopBlack implements Figure {
         }
         int size = abs(position.getX() - dest.getX());
         Cell[] steps = new Cell[size];
-        int deltaX = (position.getX() < dest.getX()) ? position.getX() + 1 : position.getX() - 1;
-        int deltaY = (position.getY() < dest.getY()) ? position.getY() + 1 : position.getY() - 1;
+        int deltaX = (position.getX() < dest.getX()) ?  1 :  - 1;
+        int deltaY = (position.getY() < dest.getY()) ?  1 :  - 1;
+        int x = position.getX();
+        int y = position.getY();
         for (int index = 0; index < size; index++) {
-            if (position.getX() < dest.getX() && position.getY() > dest.getY()) {
-                steps[index] = Cell.findBy(deltaX + index, deltaY - index);
-            } else if (position.getX() > dest.getX() && position.getY() < dest.getY()) {
-                steps[index] = Cell.findBy(deltaX - index, deltaY + index);
-            } else if (position.getX() > dest.getX() && position.getY() > dest.getY()) {
-                steps[index] = Cell.findBy(deltaX - index, deltaY - index);
-            } else {
-                steps[index] = Cell.findBy(deltaX + index, deltaY + index);
-            }
+            x += deltaX;
+            y += deltaY;
+            steps[index] = Cell.findBy(x, y);
         }
         return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        if (abs(source.getX() - dest.getX()) == abs(source.getY() - dest.getY())) {
-            return true;
-        }
-        return false;
+        return abs(source.getX() - dest.getX()) == abs(source.getY() - dest.getY());
     }
 
     @Override
